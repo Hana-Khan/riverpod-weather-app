0ed769fc7d4bceb271f814031cd3d85b0aa87be0
Hina Khan <47082795+Hana-Khan@users.noreply.github.com>
2023-11-12 16:04:18 +0500

    TemperatureFromJson

---
+++ lib/domain/entities/weather.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Temperature _$TemperatureFromJson(Map<String, dynamic> json) => Temperature(
+      value: (json['value'] as num).toDouble(),
+    );
+
