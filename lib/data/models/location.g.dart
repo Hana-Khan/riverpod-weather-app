dc2a5e549b68e6ff5fb18b7625e1abfcc995d224
Hina Khan <47082795+Hana-Khan@users.noreply.github.com>
2023-11-12 16:03:04 +0500

    LocationFromJson

--- lib/data/models/location.g.dart
+++ lib/data/models/location.g.dart
@@ -1,3 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'location.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Location _$LocationFromJson(Map<String, dynamic> json) => Location(
+      title: json['title'] as String,
+      locationType: $enumDecode(_$LocationTypeEnumMap, json['locationType']),
+      latLng: const LatLngConverter().fromJson(json['latt_long'] as String),
+      woeid: json['woeid'] as int,
+    );
+
 const _$LocationTypeEnumMap = {
   LocationType.city: 'City',
   LocationType.region: 'Region',
