7d20d842efc72488536d877ea4e53ecc0e2fa30c
Hina Khan <47082795+Hana-Khan@users.noreply.github.com>
2023-11-12 16:04:48 +0500

    WeatherFromJson entity file

--- lib/domain/entities/weather.g.dart
+++ lib/domain/entities/weather.g.dart
@@ -15,3 +15,19 @@ Map<String, dynamic> _$TemperatureToJson(Temperature instance) =>
       'value': instance.value,
     };
 
+Weather _$WeatherFromJson(Map<String, dynamic> json) => Weather(
+      condition: json['condition'] as String,
+      lastUpdated: DateTime.parse(json['lastUpdated'] as String),
+      location: json['location'] as String,
+      temperature:
+          Temperature.fromJson(json['temperature'] as Map<String, dynamic>),
+      weatherStateAbr: json['weatherStateAbr'] as String,
+      minTemp: Temperature.fromJson(json['minTemp'] as Map<String, dynamic>),
+      maxTemp: Temperature.fromJson(json['maxTemp'] as Map<String, dynamic>),
+      windSpeed: (json['windSpeed'] as num).toDouble(),
+      windDirection: (json['windDirection'] as num).toDouble(),
+      humidity: json['humidity'] as int,
+      airPressure: (json['airPressure'] as num).toDouble(),
+      date: DateTime.parse(json['date'] as String),
+    );
+
